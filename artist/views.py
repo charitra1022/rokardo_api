c3bf83f437cf1cf18f7ba179eafc53c48b8c09af
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-11-01 00:37:05 +0530

    get list of top played artists

--- artist/views.py
+++ artist/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from helper.lastfm_processor import process_artists_data
+from helper.lastfm import get_top_artists
+
+
+class GetTopArtists(APIView):
+    """
+    Returns API Response for top played artists
+    """
+
+    def get(self, request):
+        """
+        Gets list of top artists
+        :param request:
+        :return: DRF Response containing artist list
+        """
+
+        artists = process_artists_data(get_top_artists(limit=5))
+        return Response(artists, status=status.HTTP_302_FOUND)
