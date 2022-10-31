555a5c3fdbd50ea6956dbe89ddc84272fcfca53f
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-11-01 01:37:43 +0530

    separated artist data processing from batch processing

--- helper/lastfm_processor.py
+++ helper/lastfm_processor.py
@@ -3,23 +3,32 @@
 from .scrap_metadata import scrap_artist_art
 
 
-def process_artists_data(artists):
+def get_data_from_artist(artist):
     """
-    Accepts the raw data returned by the LastFM API, and returns list of processed data
-    :param artists: list(TopItem) received from the LastFM API call
+    Returns relevant data from pylast.Artist object
+    :param artist: pylast.Artist object
+    :return: dict of relevant data
+    """
+    data = {
+        "name": artist.get_name(),
+        "plays": artist.get_playcount(),
+        "mbid": artist.get_mbid(),
+        "bio": artist.get_bio_summary(),
+    }
+    data["image"] = scrap_artist_art(data["name"])
+    return data
+
+
+def process_artists_data(topItems):
+    """
+    Accepts the list of raw data returned by the LastFM API, and returns list of processed data
+    :param topItems: list(TopItem) received from the LastFM API call
     :return: list of dict containing processed data
     """
 
     processed_data = []
 
-    for a in artists:
-        artist = {
-            "name": a.item.get_name(),
-            "plays": a.item.get_playcount(),
-            "mbid": a.item.get_mbid(),
-            "bio": a.item.get_bio_summary(),
-        }
-        artist["image"] = scrap_artist_art(artist["name"])
-        processed_data.append(artist)
+    for i in topItems:
+        processed_data.append(get_data_from_artist(i.item))
 
     return processed_data
